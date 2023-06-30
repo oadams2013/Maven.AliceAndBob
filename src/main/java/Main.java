2f39dd4e1297191bc34123a7218285d4e296da82
Olamide <olamide@Zipcoders-MacBook-Pro-26.local>
2023-06-30 15:02:38 -0400

    I'm done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
 
+        System.out.println("Please Enter Your Name Below ");
+        name = scanner.nextLine();
+        String name1 = "Alice";
+        String name2 = "Bob";
+
+        if (name.equals(name1)) {
+            System.out.println("Wassup, Alice?!");
+        } else if (name.equals(name2)) {
+            System.out.println("What it do? Bob! ");
+        } else {
+            System.out.println("Who Dis? ");
+        }
     }
 }
+
+
